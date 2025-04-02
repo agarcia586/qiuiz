aff801a3658c4921d43358dfa220eba6c60a6de5
agarcia586 <agarcia586@cps.edu>
2025-04-02 16:23:57 +0000

    BOOMSHAKALA

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
-<script>
+
     document.querySelector(".timebutton").addEventListener("click", function() {
-         let timeInput = Number(document.querySelector(".time").value);
-      if (timeInput > = 1960 && timeInput < = 1999) {
-            window.location.href = "https://www.ebay.com/";
-            } else if (timeInput >= 2000 && timeInput <= 2025) {
-            window.location.href = "https://www.amazon.com/"; 
-						} else {document.querySelector(".timemessage").innerHTML = "Please enter a year between 1960-2025!";
-}
-    
-    }
-      
+let timeInput = Number(document.querySelector(".time").value);
+         if (timeInput >= 1960 && timeInput <= 1999) {
+            window.location.href = "3q1.html";
+         } else if (timeInput >= 2000 && timeInput <= 2025) {
+            window.location.href = "3q2.html";
+         }
     });
-</script>
